b17bcd52a54624a9efb70a96ed24c067b2216610
Panchev <neno.panchev@gmail.com>
2022-05-13 15:56:32 +0300

    added backup and update functions from web and fixed admin panel nav

--- daily-nail/src/main/java/project/dailynail/init/DailyNailAppInit.java
+++ daily-nail/src/main/java/project/dailynail/init/DailyNailAppInit.java
@@ -1,11 +1,8 @@
 package project.dailynail.init;
 
-import com.google.gson.Gson;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import project.dailynail.constants.GlobalConstants;
 import project.dailynail.services.*;
-import project.dailynail.utils.FileIOUtil;
 
 @Component
 public class DailyNailAppInit implements CommandLineRunner {
@@ -13,37 +10,21 @@ public class DailyNailAppInit implements CommandLineRunner {
     private final UserRoleService userRoleService;
     private final SubcategoryService subcategoryService;
     private final CategoryService categoryService;
-    private final Gson gson;
-    private final FileIOUtil fileIOUtil;
-    private final ArticleService articleService;
-    private final CommentService commentService;
 
-    public DailyNailAppInit(UserService userService, UserRoleService userRoleService, SubcategoryService subcategoryService, CategoryService categoryService, Gson gson, FileIOUtil fileIOUtil, ArticleService articleService, CommentService commentService) {
+
+    public DailyNailAppInit(UserService userService, UserRoleService userRoleService, SubcategoryService subcategoryService, CategoryService categoryService) {
         this.userService = userService;
         this.userRoleService = userRoleService;
         this.subcategoryService = subcategoryService;
         this.categoryService = categoryService;
-        this.gson = gson;
-        this.fileIOUtil = fileIOUtil;
-        this.articleService = articleService;
-        this.commentService = commentService;
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         this.userRoleService.seedUserRoles();
         this.userService.seedUsers();
         this.categoryService.seedCategories();
         this.subcategoryService.seedSubcategories();
 
-        // Back up the data
-//        fileIOUtil.write(gson.toJson(userService.exportUsers()), USERS_FILE_PATH);
-//        fileIOUtil.write(gson.toJson(articleService.exportArticles()), ARTICLES_FILE_PATH);
-//        fileIOUtil.write(gson.toJson(commentService.exportComments()), GlobalConstants.COMMENTS_FILE_PATH);
-
-        // Update data from backups
-//        userService.seedNonInitialUsers();
-//        articleService.seedArticles();
-//        commentService.seedComments();
     }
 }
--- daily-nail/src/main/java/project/dailynail/web/AdminController.java
+++ daily-nail/src/main/java/project/dailynail/web/AdminController.java
@@ -1,18 +1,28 @@
 package project.dailynail.web;
 
+import com.google.gson.Gson;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import project.dailynail.models.binding.ArticleEditBindingModel;
+import project.dailynail.constants.GlobalConstants;
 import project.dailynail.models.binding.UserUpdateRoleBindingModel;
 import project.dailynail.models.dtos.UserRoleDto;
 import project.dailynail.services.ArticleService;
+import project.dailynail.services.CommentService;
 import project.dailynail.services.UserService;
+import project.dailynail.utils.FileIOUtil;
 
 import javax.validation.Valid;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static project.dailynail.constants.GlobalConstants.*;
+import static project.dailynail.constants.GlobalConstants.ARTICLES_FILE_PATH;
+import static project.dailynail.constants.GlobalConstants.USERS_FILE_PATH;
 
 @Controller
 @RequestMapping("/admin")
@@ -20,11 +30,17 @@ public class AdminController {
     private final ArticleService articleService;
     private final UserService userService;
     private final ModelMapper modelMapper;
+    private final FileIOUtil fileIOUtil;
+    private final Gson gson;
+    private final CommentService commentService;
 
-    public AdminController(ArticleService articleService, UserService userService, ModelMapper modelMapper) {
+    public AdminController(ArticleService articleService, UserService userService, ModelMapper modelMapper, FileIOUtil fileIOUtil, Gson gson, CommentService commentService) {
         this.articleService = articleService;
         this.userService = userService;
         this.modelMapper = modelMapper;
+        this.fileIOUtil = fileIOUtil;
+        this.gson = gson;
+        this.commentService = commentService;
     }
 
 
@@ -57,6 +73,28 @@ public class AdminController {
         userService.updateRole(modelMapper.map(userUpdateRoleBindingModel, UserRoleDto.class));
 
         return "redirect:/admin/accounts";
+    }
+
+    @GetMapping("/data")
+    public String data() {
+        return "data";
+    }
+
+    @GetMapping("/data/backup")
+    public String backup() throws IOException {
+        fileIOUtil.write(gson.toJson(userService.exportUsers()), USERS_FILE_PATH);
+        fileIOUtil.write(gson.toJson(articleService.exportArticles()), ARTICLES_FILE_PATH);
+        fileIOUtil.write(gson.toJson(commentService.exportComments()), COMMENTS_FILE_PATH);
+
+        return "redirect:/admin";
+    }
+
+    @PostMapping("/data/import")
+    public String importConfirm() throws FileNotFoundException {
+        userService.seedNonInitialUsers();
+        articleService.seedArticles();
+        commentService.seedComments();
+        return "redirect:/admin";
     }
 
     @ModelAttribute("userUpdateRoleBindingModel")
