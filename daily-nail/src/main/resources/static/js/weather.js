51dc661ac95873b596de19fba7fdf5ccb0e8d8b3
Panchev <neno.panchev@gmail.com>
2022-10-02 17:04:11 +0300

    Switched Weather to show Plovdiv after trying few things

--- daily-nail/src/main/resources/static/js/weather.js
+++ daily-nail/src/main/resources/static/js/weather.js
@@ -3,16 +3,11 @@ const boxImgB = document.getElementById('box-b-img');
 const boxTempA = document.getElementById('box-a-temp');
 const boxTempB = document.getElementById('box-b-temp');
 
-console.log("OK")
-
-fetch("http://api.openweathermap.org/data/2.5/weather?q=Sofia&appid=e80b8bd999a5a20029a930973b3e8a4b")
+fetch("https://api.openweathermap.org/data/2.5/weather?q=Plovdiv&appid=e80b8bd999a5a20029a930973b3e8a4b")
     .then(data => data.json())
     .then(info => {
-        console.log(info);
-
         //Formula Kelvin to Celsius 299K − 273.15 = 25.85°C
         boxTempA.innerText = Math.round(info.main.temp - 273.15);
         boxImgA.src = '/images/weather-icons/' + info.weather[0].icon + '.png';
 
     })
-
