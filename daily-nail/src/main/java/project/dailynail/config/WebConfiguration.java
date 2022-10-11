0fc5cffd025454e7c45e454d429c8f4e4fa19434
Panchev <neno.panchev@gmail.com>
2022-10-11 16:56:02 +0300

    Added 2nd interceptor for maintenanc page

--- daily-nail/src/main/java/project/dailynail/config/WebConfiguration.java
+++ daily-nail/src/main/java/project/dailynail/config/WebConfiguration.java
@@ -3,19 +3,23 @@ package project.dailynail.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import project.dailynail.web.interceptors.MaintenanceInterceptor;
 import project.dailynail.web.interceptors.StatsInterceptor;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
     private final StatsInterceptor statsInterceptor;
+    private final MaintenanceInterceptor maintenanceInterceptor;
 
-    public WebConfiguration(StatsInterceptor statsInterceptor) {
+    public WebConfiguration(StatsInterceptor statsInterceptor, MaintenanceInterceptor maintenanceInterceptor) {
         this.statsInterceptor = statsInterceptor;
+        this.maintenanceInterceptor = maintenanceInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(statsInterceptor);
+        registry.addInterceptor(maintenanceInterceptor);
     }
 }
