98069eb0979914dadd09e40e5981fbf1ed0865b2
Neno <neno.panchev@gmail.com>
2023-04-17 15:06:26 +0300

    ordered logs by time

--- daily-nail/src/main/java/project/dailynail/repositories/LogRepository.java
+++ daily-nail/src/main/java/project/dailynail/repositories/LogRepository.java
@@ -1,9 +1,15 @@
 package project.dailynail.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import project.dailynail.models.entities.LogEntity;
 
+import java.util.List;
+
 @Repository
 public interface LogRepository extends JpaRepository<LogEntity, String> {
+    @Query("SELECT l FROM LogEntity l " +
+            "ORDER BY l.time DESC ")
+    List<LogEntity> findAllOrderByTimeDesc();
 }
